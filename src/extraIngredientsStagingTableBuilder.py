86e4a2a0892f7f4df403f7c499e89dede23c0624
Cincle <37715797+Cincle@users.noreply.github.com>
2021-03-29 08:36:29 +0200

    Price repair logging, ingredient name error logging

--- src/extraIngredientsStagingTableBuilder.py
+++ src/extraIngredientsStagingTableBuilder.py
@@ -1,14 +1,20 @@
 import os
+import textwrap
 
 import pandas as pd
 import src
+from src.logger import Logger
+from src.price_cleanup_helper import PriceCleanupHelper
 
 
 class ExtraIngredientsStagingTableBuilder:
 
-    # Constructor to setup shop importer
+    # Constructor to setup extra ingredient importer
     def __init__(self, filename):
         self.filename = filename
+
+    def __indent(self, text, amount, ch=' '):
+        return textwrap.indent(text, amount * ch)
 
     def process(self):
         print('Building Extra Ingredients Staging Table')
@@ -17,12 +23,10 @@ class ExtraIngredientsStagingTableBuilder:
             engine = src.dbEbgine().get_db_engine()
 
             ingredienten_date_frame = pd.read_csv(filepath, sep=';')
+            PriceCleanupHelper().clean_prices(ingredienten_date_frame, 'Extra Price', self.filename)
 
-            ingredienten_date_frame['Extra Price'] = ingredienten_date_frame['Extra Price'].str.replace(r"[a-zA-Z€ ]", '', regex=True)
-            ingredienten_date_frame["Extra Price"] = pd.to_numeric(ingredienten_date_frame["Extra Price"])
             # uniform capitalization of ingredient names.
             ingredienten_date_frame['Ingredient'] = ingredienten_date_frame['Ingredient'].str.title()
 
             ingredienten_date_frame.to_sql('extra_ingredienten_ghost', con=engine, if_exists='replace')
             print('Extra Ingredients staging table done\n')
-
