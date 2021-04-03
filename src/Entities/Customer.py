9b9ac521b57ab8d11b928b81d9e75e53f34c0643
Derek <derekmeuwissen@gmail.com>
2021-04-03 22:13:55 +0200

    Order data

--- src/Entities/Customer.py
+++ src/Entities/Customer.py
@@ -5,28 +5,27 @@ def createOrUpdateCustomer(database, email, name, phoneNr, addressId):
     with database.cursor as cursor:
         query = 'SELECT id FROM customer WHERE UPPER(email_adress) LIKE UPPER(?)'
         args = (email)
-        for row in cursor.execute(query, args):
+        for row in cursor.execute(query, args).fetchall():
             query = 'UPDATE customer SET name = ?, phone_nr = ? WHERE id = ?'
             args = [name, phoneNr, row.id]
             cursor.execute(query, args)
 
-            query = """IF NOT EXISTS(SELECT * FROM customer_adress WHERE adress_id = ? AND customer_id = ?)
+            query = """IF NOT EXISTS(SELECT * FROM customer_adress WHERE other_adress_id = ? AND customer_id = ?)
                         BEGIN
-                        INSERT INTO customer_adress(adress_id, customer_id) VALUES(?, ?) 
+                        INSERT INTO customer_adress(other_adress_id, customer_id) VALUES(?, ?) 
                         END
                         """
             args = [addressId, row.id, addressId, row.id]
-            # cursor.execute(query, args)
-            database.conn.commit()
+            cursor.execute(query, args)
             return row.id
 
-        query = 'INSERT INTO customer(name, phone_nr, email_adress) VALUES(?, ?, ?)'
+        query = '''INSERT INTO customer(name, phone_nr, email_adress)
+                   OUTPUT inserted.ID
+                   VALUES(?, ?, ?)'''
         args = (name, phoneNr, email)
-        cursor.execute(query, args)
-
-        id = cursor.execute("SELECT @@IDENTITY AS ID;").fetchone()[0]
+        id = cursor.execute(query, args).fetchone()[0]
         return id
 
 
 
-        return None
+
